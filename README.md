4a89efb6e7c816a064a4ab7503de3cedab07f1ba
Selami Demir <selamidemir@gmail.com>
2023-01-27 18:13:05 +0300

    README.md dosyası eklendi. Yemek detay sayfasına Youtube düğmesi eklenmedi. Çünkü veriden gelen bir sorun bulunmakta.

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native İle Yemek Listesi Uygulaması
+
+Bu uygulama sadece eğitim amaçlıdır. Axios ile birlikte API çalışması yapılmıştır. 
+
+API kaynağına [https://www.themealdb.com/api.php](https://www.themealdb.com/api.php) adersinde ulaşabilirsiniz.
--- src/pages/MealDetails/MealDetails.js
+++ src/pages/MealDetails/MealDetails.js
@@ -14,12 +14,18 @@ function MealDetails({route}) {
     const url = Config.API_MEAL_DESC + mealID;
     const { error, loading, data} = useFetch(url);
 
-    if (error) <Error message={error} />
-    if (loading) <Loading />
+    if (error) return <Error message={error} />
+    if (loading) return <Loading />
 
-    const sdata = JSON.stringify(data);
-    const ddata = JSON.parse(sdata);
-    const meals = ddata.meals[0];
+    try {
+        const sdata = JSON.stringify(data);
+        const ddata = JSON.parse(sdata);
+        const meals = ddata.meals[0];
+        console.log("MEALS : ", meals.strMeal)
+    } catch (error) {
+        return <Error message={error.message} />
+        meals = {};
+    }
     
 
     return (
@@ -27,7 +33,6 @@ function MealDetails({route}) {
             <Image style={style.image} source={{uri: meals.strMealThumb}} />
             <Text style={style.title}>{ meals.strMeal }</Text>
             <Text style={style.description}>{meals.strInstructions}</Text>
-            <Button onPress={watchOnYoutube}>Watch on Youtube</Button>
         </SafeAreaView>
     )
 }
